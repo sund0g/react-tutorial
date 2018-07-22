f990129aa634d0bd87074fcba844be48f47e7778
Scott Estes <scottes@gmail.com>
2018-07-22 15:38:00 -0700

    working with imports/exports. At end of Section 5 Lecture 52.

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,1 +1,14 @@
+//import './utils.js'; //This simply import the file, but there is no access to any objects it contains.
+
+import anythingIWant, { square, add } from './utils.js'; // only named imports are in {}.
+
 console.log('app.js is running!');
+console.log(square(4));
+console.log(add(100, 23));
+console.log(anythingIWant(100, 81)); // Default functions can be called anything when imported.
+
+import isSenior, { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(21));
+console.log(canDrink(18));
+console.log(isSenior(64));
