ee36541460b22c87af0b14e95749d08e9016c96e
Scott Estes <scottes@gmail.com>
2018-07-22 17:51:15 -0700

    moving components into files. At end of Section 6 Lecture 55. (#11)

---
+++ indecision/src/components/AddOption.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class AddOption extends React.Component { // can set up default class like this, but not with other objects.
+
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        }
+    }
+    handleAddOption(e) {
+        e.preventDefault(); //Don't want to reload entire page; only what has changed.
+
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
+
+        this.setState(() => ({error})); // Implicit return
+//            return { error };   // If what we're returning and the variable name are the same
+                                // we don't have to use e.g. error: error.
+
+        if(!error) { // Clear the value in the listbox if no error.
+            e.target.elements.option.value = '';
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleAddOption}>
+                  <input type="text" name="option" />
+                  <button>Add Option</button>
+                </form>
+            </div>
+        );
+    }
+}
