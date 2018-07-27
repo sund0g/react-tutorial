2fd64b0ffca1c5d9795e5ae67bc41eb503d06c32
Scott Estes <scottes@gmail.com>
2018-07-27 14:43:48 -0700

    At end of Section 8 Lecture 62 (#15)

--- indecision/src/components/header.js
+++ indecision/src/components/header.js
@@ -1,14 +1,12 @@
-// Stateless functional component
-
 import React from 'react';
 
-const Header = (props) => { // Was a class; converted to stateless component
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
-        </div>
-    );
-}
+// Was a class; converted to stateless component
+
+const Header = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+    </div>
+)
 
 export default Header;
