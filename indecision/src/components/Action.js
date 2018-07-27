b63e1492104ee593d8b1beec8e45650d73f04259
Scott Estes <scottes@gmail.com>
2018-07-27 14:42:14 -0700

    At end of Section 8 Lecture 62

--- indecision/src/components/Action.js
+++ indecision/src/components/Action.js
@@ -2,17 +2,15 @@
 
 import React from 'react';
 
-const Action = (props) => { // Was a class; converted to stateless component
-    return (
-        <div>
-            <button
-                onClick={props.handlePick}
-                disabled={!props.hasOptions}
-            >
-                What should I do?
-            </button>
-        </div>
-    );
-};
+const Action = (props) => (
+    <div>
+        <button
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}
+        >
+            What should I do?
+        </button>
+    </div>
+)
 
 export default Action
--- indecision/src/components/Option.js
+++ indecision/src/components/Option.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Option = (props) => { // Was a class; converted to stateless component
-    return (
-        <div>
-            {props.optionText}
-            <button
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                remove
-            </button>
-        </div>
-    );
-};
+// Was a class; converted to stateless component
+
+const Option = (props) => (
+    <div>
+        {props.optionText}
+        <button
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        >
+            remove
+        </button>
+    </div>
+)
 
 export default Option; // Doing this instead of inline default for consts. Otherwise, in the React dev tools, calls to Option will be id'ed as 'unknown'.
