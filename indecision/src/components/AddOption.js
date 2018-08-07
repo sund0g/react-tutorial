5b0d029a83de64768cc834fd9c5ec8d54128893c
Scott Estes <scottes@gmail.com>
2018-08-06 17:41:09 -0700

    adding buttons styles. At end of Section 8 Lecture 68.

--- indecision/src/components/AddOption.js
+++ indecision/src/components/AddOption.js
@@ -27,7 +27,7 @@ export default class AddOption extends React.Component { // can set up default c
                 {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                   <input type="text" name="option" />
-                  <button>Add Option</button>
+                  <button className="button">Add Option</button>
                 </form>
             </div>
         );
