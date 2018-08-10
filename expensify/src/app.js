ae01a663480fec70b56c828d2d82295e8ada6194
Scott Estes <scottes@gmail.com>
2018-08-10 16:19:23 -0700

    adding linking between routes. Section 9 Lecture 79.

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
+const ExpenseDashboardPage = () => ( // Implicit returns
     <div>
         This is from my dashboard component
     </div>
@@ -29,9 +29,20 @@ const HelpPage = () => (
 );
 
 const NotFoundPage = () => (
+    // React Route Link method tells JavaScript to override server-side refresh with client-side refresh.
     <div>
-        404!
+        404 <Link to="/">Go home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 // Using React Route methods,
@@ -42,15 +53,20 @@ const NotFoundPage = () => (
 
 // The React Route method Switch works just like a regular switch statement.
 
+// Note BrowserRouter method can have only a single root element, hence the use of <div></div>.
+
 const routes = (
     <BrowserRouter>
-        <Switch>
-          <Route path="/" component={ExpenseDashboardPage} exact={true} />
-          <Route path="/create" component={AddExpensePage} />
-          <Route path="/edit" component={EditExpensePage} />
-          <Route path="/help" component={HelpPage} />
-          <Route component={NotFoundPage} />
-      </Switch>
+        <div>
+            <Header/>
+            <Switch>
+              <Route path="/" component={ExpenseDashboardPage} exact={true} />
+              <Route path="/create" component={AddExpensePage} />
+              <Route path="/edit" component={EditExpensePage} />
+              <Route path="/help" component={HelpPage} />
+              <Route component={NotFoundPage} />
+          </Switch>
+        </div>
     </BrowserRouter>
 );
 
