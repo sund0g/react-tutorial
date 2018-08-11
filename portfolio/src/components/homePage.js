85c62111d11edb29201b50b4633fbd70db94a181
Scott Estes <scottes@gmail.com>
2018-08-11 12:10:24 -0700

    Adding portfolio app. Section 9 Lecture 82.

---
+++ portfolio/src/components/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div>
+        <h1>Welcome</h1>
+        <p>This is my home page. Take a look around.</p>
+    </div>
+)
+
+export default HomePage;
---
+++ portfolio/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+// console.log(__dirname); // Used to find absolute path needed for config below.
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: { // https://webpack.js.org/configuration/module/#module-rules
+        // set up our module as follows, for any .js files in our app, load them, with babel-loader, except for the auto-generated node_modules.
+        rules: [{
+            loader: 'babel-loader', // here we only need a single loader.
+            test: /\.js$/, // include any file with .js extension.
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,   // include any files with .scss  or .css extension.
+                                // the '?' makes the 's' optional so webpack will include both.
+            use: [ // here we need multiple loaders. Do this with an array.
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map', // https://webpack.js.org/configuration/devtool/
+    devServer: { // https://webpack.js.org/configuration/dev-server/#src/components/Sidebar/Sidebar.jsx
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true    // We're telling devServer to handle routing via client side code.
+                                    // therefore it should return index.html for all 404 routes.
+    }
+};
