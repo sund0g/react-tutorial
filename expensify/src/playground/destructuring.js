ee7b0e5a895fe4583d299a58d6b323dc0f447fce
Scott Estes <scottes@gmail.com>
2018-08-14 19:19:25 -0700

    adding array destructuring. Section 10 Lecture 89.

--- expensify/src/playground/destructuring.js
+++ expensify/src/playground/destructuring.js
@@ -1,8 +1,11 @@
+//
+// Object destructuring
+//
 const person = {
     name: 'Bugs',
     age: 42,
     location: {
-        city: 'Burbank',
+        city1: 'Burbank',
         temp: 92
     }
 };
@@ -17,6 +20,8 @@ const person = {
 // 2. Default values using name = 'Anonymous' When name is not defined in person above, the default will kick in.
 // 3. Combining 1 and 2 using name: firstName = 'Anonymous'
 //
+// NOTE: for object destructuring, use {}.
+//
 const { name: firstName = 'Anonymous', age } = person;
 
 // The above ES6 syntax is equivalent to,
@@ -25,12 +30,12 @@ const { name: firstName = 'Anonymous', age } = person;
 
 console.log(`${firstName} is ${age}.`);
 
-const { temp: temperature, city } = person.location;
+const { temp: temperature, city1 } = person.location;
 
 // Now can reference the destructured variables as needed.
 
-if (temperature && city) {
-    console.log(`It's ${temperature} in ${city}`);
+if (temperature && city1) {
+    console.log(`It's ${temperature} in ${city1}`);
 }
 
 // Challenge
@@ -49,3 +54,26 @@ const { name: publisherName = 'self-published' } = book.publisher;
 // REMEMBER TO USE FREAKING `` in console.log. I HATE FREAKING ``!!!
 //
 console.log(`${publisherName}`);
+
+//
+// Array destructuring
+//
+// NOTE: for array destructuring, use [].
+// Also, the references are matched in order rather than name, like for objects.
+// Additionally, there is no renaming syntax for arrays.
+// There is a default state.
+//
+ const address = ['123 Acme Drive', 'Burbank', 'California', '91522'];
+
+ const [, city2, state = 'Florida', zip] = address; // Because of the ordering, this says, skip to first array element.
+
+console.log (`You are in ${city2}, ${state}`);
+
+//
+// Challenge
+//
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, ,mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
